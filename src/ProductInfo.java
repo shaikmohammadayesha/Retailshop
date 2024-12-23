4b957bac6567fd5456bbcf0099a6c40e0a2aaae1
Ayesha <shaikmohammadayesha@gmail.com>
2024-12-23 15:30:36 +0530

    includes units available in shop.

--- src/ProductInfo.java
+++ src/ProductInfo.java
@@ -1,16 +1,16 @@
 //This class ProductInfo is to store the product details name of the product, tax index that it belongs and the unit price of the product.
 //This class is used to create object and used as a dataType.
 
-class ProductInfo{
- String name;
- int taxIndex;
- int unitPrice;
-
-
-ProductInfo(String name, int index, int price){
-this.name = name;
-this.taxIndex = index;
-this.unitPrice = price;
-}
+class ProductInfo {
+    String name;
+    int taxIndex;
+    int unitPrice;
+    int unitsAvailable;
+    ProductInfo(String name, int index, int price, int unitsAvailable) {
+        this.name = name;
+        this.taxIndex = index;
+        this.unitPrice = price;
+        this.unitsAvailable = unitsAvailable;
+    }
 
 }
